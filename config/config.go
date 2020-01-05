620374e7985bb74adf075725803261b49912c55c
hatappi <hatappi@hatappi.me>
2020-01-06 00:18:10 +0900

    feat: add validator for config

--- config/config.go
+++ config/config.go
@@ -2,6 +2,7 @@
 package config
 
 import (
+	"github.com/go-playground/validator/v10"
 	"github.com/spf13/viper"
 )
 
@@ -12,15 +13,21 @@ type Config struct {
 
 // PomodoroConfig config for pomodoro
 type PomodoroConfig struct {
-	WorkSec       int `mapstructure:"work_sec"`
-	ShortBreakSec int `mapstructure:"short_break_sec"`
-	LongBreakSec  int `mapstructure:"long_break_sec"`
+	WorkSec       int `mapstructure:"work_sec" validate:"gt=0,lte=3600"`
+	ShortBreakSec int `mapstructure:"short_break_sec" validate:"gt=0,lte=3600"`
+	LongBreakSec  int `mapstructure:"long_break_sec" validate:"gt=0,lte=3600"`
 }
 
 // GetConfig get Config
 func GetConfig() (*Config, error) {
 	var c Config
 	err := viper.Unmarshal(&c)
+	if err != nil {
+		return nil, err
+	}
+
+	validate := validator.New()
+	err = validate.Struct(&c)
 	if err != nil {
 		return nil, err
 	}
