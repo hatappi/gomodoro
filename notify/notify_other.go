04c462ee65f7608acaa374b6f91b989ea1cb05b0
hatappi <hatappi@hatappi.me>
2020-01-09 22:15:27 +0900

    fix: lint

--- notify/notify_other.go
+++ notify/notify_other.go
@@ -1,11 +1,13 @@
 // +build !darwin
 
+// Package notify notification
 package notify
 
 import (
 	"golang.org/x/xerrors"
 )
 
+// Notify return unsupported error
 func Notify(title, message string) error {
 	return xerrors.New("unsupported notification")
 }
