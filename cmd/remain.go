27ad5efd2fcecb9ef770357a9e4454bf21e7757c
hatappi <hatappi@hatappi.me>
2020-01-12 12:41:46 +0900

    chore: remove unnessary comment

--- cmd/remain.go
+++ cmd/remain.go
@@ -38,14 +38,4 @@ var remainCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(remainCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// remainCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// remainCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
