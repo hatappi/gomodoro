3cfcd7fa1f49c1aa2382e4879e772bd0108c8d05
hatappi <hatappi@hatappi.me>
2020-01-12 12:41:15 +0900

    fix: remain Usage

--- cmd/remain.go
+++ cmd/remain.go
@@ -13,13 +13,7 @@ import (
 // remainCmd represents the remain command
 var remainCmd = &cobra.Command{
 	Use:   "remain",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "get remain time",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		config, err := config.GetConfig()
 		if err != nil {
