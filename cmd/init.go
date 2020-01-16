fc6d4db57830538d8e5f64ee74db48ec25722980
hatappi <hatappi@hatappi.me>
2020-01-17 00:26:32 +0900

    feat: initilize init command

---
+++ cmd/init.go
@@ -0,0 +1,22 @@
+// Package cmd has initCmd defined
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// initCmd represents the init command
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "init gomodoro config file",
+	Long:  "init gomodoro config file",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("init called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+}
