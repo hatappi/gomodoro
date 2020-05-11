deb0b59ab3e7f136842ba92c907460d811721b1a
hatappi <hatappi@hatappi.me>
2020-05-12 00:40:04 +0900

    add reviewdog

--- tools.go
+++ tools.go
@@ -5,4 +5,5 @@ package tools
 // https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
 import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/reviewdog/reviewdog/cmd/reviewdog"
 )
