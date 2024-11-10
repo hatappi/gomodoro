7904ec3beb1276e4bb28e16dd4a1e38e1f656cdc
hatappi <3453977+hatappi@users.noreply.github.com>
2024-11-10 15:54:59 +0900

    Toggl API v8 migration

--- internal/toggl/time_entry.go
+++ internal/toggl/time_entry.go
@@ -19,7 +19,8 @@ type TimeEntry struct {
 	CreatedWith string   `json:"created_with"`
 	Start       string   `json:"start"`
 	Duration    int      `json:"duration"`
-	PID         int      `json:"pid"`
+	ProjectID   int      `json:"project_id"`
+	WorkspaceID int      `json:"workspace_id"`
 	TAGS        []string `json:"tags"`
 }
 
@@ -30,23 +31,18 @@ func (c *Client) PostTimeEntry(ctx context.Context, desc string, start time.Time
 		CreatedWith: appName,
 		Start:       start.Format("2006-01-02T15:04:05Z07:00"),
 		Duration:    duration,
-		PID:         c.projectID,
+		WorkspaceID: c.workspaceID,
+		ProjectID:   c.projectID,
 	}
 
-	body := &struct {
-		TimeEntry *TimeEntry `json:"time_entry"`
-	}{
-		TimeEntry: timeEntry,
-	}
-
-	jsonBytes, err := json.Marshal(body)
+	jsonBytes, err := json.Marshal(timeEntry)
 	if err != nil {
 		return err
 	}
 	req, err := http.NewRequestWithContext(
 		ctx,
 		http.MethodPost,
-		"https://api.track.toggl.com/api/v8/time_entries",
+		fmt.Sprintf("https://api.track.toggl.com/api/v9/workspaces/%d/time_entries", c.workspaceID),
 		bytes.NewBuffer(jsonBytes),
 	)
 	if err != nil {
@@ -69,7 +65,7 @@ func (c *Client) PostTimeEntry(ctx context.Context, desc string, start time.Time
 		if err != nil {
 			return fmt.Errorf("failed to read body: %w", err)
 		}
-		return fmt.Errorf("request failed. detail: %s", body)
+		return fmt.Errorf("request failed. status: %d, detail: %s", res.StatusCode, body)
 	}
 	return nil
 }
